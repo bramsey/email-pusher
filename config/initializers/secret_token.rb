4381d99d47292470bd7c41f1a7ce9c1a67b9e6e1
Bill Ramsey <bill@vybly.com>
2011-08-01 15:28:59 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pusher::Application.config.secret_token = '4f154a2055350a2cdfac2c45e1ce8c5886b2ae2b6d98843d98ab671d4b71d7463d58b652fe57df1726ed9c5c17463af922ddc8ae47b440232fe402409924a61b'
