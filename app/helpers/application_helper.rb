f3a515eba9e2eaa26dc943e6ef9a5e462e2ce6bd
Bill Ramsey <bill@vybly.com>
2011-08-02 23:57:50 -0400

    updated nav style

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,23 @@ module ApplicationHelper
     check_box_tag( "listening", "listening", listening,
       {'data-href' => toggle_listening_user_path(current_user), :class => 'checkable' })
   end
+  
+  def siteNav
+    if user_signed_in?
+      link("Accounts", root_path) + " - " +
+      link("Contacts", contacts_path)
+    else
+      #put logged_out links here.
+      #link("About", about_path) +
+      #link("Contact", contact_path)
+    end
+  end
+  
+  def link(label, path)
+    if request.request_uri == path
+      link_to label, path, :class => "selected"
+    else
+      link_to label, path
+    end
+  end
 end
