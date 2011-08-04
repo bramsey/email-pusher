2cc5e1a64072392107b2de2679c4c6b1b51b9bb3
Bill Ramsey <bill@vybly.com>
2011-08-04 12:59:57 -0400

    ajaxified notification service crud and flashes

--- app/controllers/notification_services_controller.rb
+++ app/controllers/notification_services_controller.rb
@@ -3,6 +3,8 @@ class NotificationServicesController < ApplicationController
   before_filter :authorized_user
   require 'notifo'
   
+  respond_to :html, :js
+  
   def index
     @user = User.find(params[:user_id])
     @notification_services = @user.notification_services
@@ -22,15 +24,14 @@ class NotificationServicesController < ApplicationController
   def create
     @notification_service  = NotificationService.create(params[:notification_service])
     if @notification_service.save
-      flash[:success] = "Service created!"
+      flash.now[:success] = "Service created!"
       notifo = Notifo.new("vybly","notifo_key")
       response = notifo.subscribe_user(@notification_service.username)
       RAILS_DEFAULT_LOGGER.error response
       current_user.update_attribute(:default_notification_service_id, @notification_service.id) if current_user.notification_services.all.length == 1
-      redirect_to root_path
-    else
-      render 'pages/home'
     end
+    
+    respond_with @notification_service
   end
   
   def edit
@@ -39,11 +40,14 @@ class NotificationServicesController < ApplicationController
   end
   
   def update
-    @NotificationService = NotificationService.find(params[:id])
-    if @NotificationService.update_attributes(params[:NotificationService])
-      flash[:success] = "Notification Service updated."
+    @notification_service = NotificationService.find(params[:id])
+    if params[:notifo_service][:username] == ""
+      flash[:success] = "Notification Service Deleted"
+      destroy
+    elsif @notification_service.update_attributes(params[:notifo_service])
+      flash.now[:success] = "Notification Service updated."
       #ToDo: insert trigger for notification here.
-      redirect_to root_path
+      respond_with @notification_service
     else
       flash[:error] = "Notification Service not updated."
       render 'edit'
@@ -54,7 +58,7 @@ class NotificationServicesController < ApplicationController
   def destroy
     current_user.update_attribute(:default_notification_service, nil) if current_user.default_notification_service == @notification_service
     @notification_service.destroy
-    redirect_back_or user_notification_services_path(current_user) 
+    redirect_back_or root_path
   end
   
   private
