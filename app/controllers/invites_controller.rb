eda2781cd25d84a43cc53f3018be85501fda114e
Bill Ramsey <bill@vybly.com>
2011-08-22 12:16:47 -0400

    cleaned up invites controller

--- app/controllers/invites_controller.rb
+++ app/controllers/invites_controller.rb
@@ -2,6 +2,7 @@ class InvitesController < ApplicationController
   
   before_filter :authenticate_user!,      :except => [:new, :create, :init]
   before_filter :admin_user, :only => [:index]
+  before_filter :load, :only => [:destroy, :approve, :disapprove]
   
   def index
     @invites = Invite.all
@@ -18,24 +19,24 @@ class InvitesController < ApplicationController
     @invite = Invite.new(params[:invite])
     if @invite.save
       flash[:success] = "Thanks for your interest!"
-      InviteMailer.thank_you_email( @invite ).deliver
+      InviteMailer.thank_you_email( @invite ).deliver # Make asynch.
     else
-      flash[:error] = "There was an error submitting your email address, please check your entry and try again."
+      flash[:error] = %q{ There was an error submitting your email address, 
+        please check your entry and try again. }
     end
     
-    redirect_to root_path # possibly to a thank you page for analytics goal tracking.
+    redirect_to root_path # Redirect to a thank you page for analytics goal tracking.
   end
 
   def destroy
-    Invite.find(params[:id]).destroy
-    flash[:success] = "Invite destroyed."
+    @invite.destroy ?
+      flash[:success] = "Invite destroyed." :
+      flash[:error] = "Invite could not be destroyed."
 
     redirect_to invites_path
   end
   
   def approve
-    @invite = Invite.find(params[:id])
-    
     @invite.update_attribute(:approved, true)
     
     InviteMailer.approval_accepted_email( @invite ).deliver
@@ -44,11 +45,13 @@ class InvitesController < ApplicationController
   end
   
   def disapprove
-    @invite = Invite.find(params[:id])
     @invite.update_attribute(:approved, false)
     redirect_to invites_path
   end
   
   private
-    
+  
+    def load
+      @invite = Invite.find(params[:id])
+    end
 end
