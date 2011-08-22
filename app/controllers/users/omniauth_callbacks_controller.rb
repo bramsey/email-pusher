e7c26ae09f7938055d2799f77ab14265eec1c80e
Bill Ramsey <bill@vybly.com>
2011-08-22 13:32:46 -0400

    replaced appropriate double quotes with single quotes in omniauth controller

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,17 +1,16 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
   def open_id
-    @user = User.find_for_open_id(env["omniauth.auth"], current_user)
+    @user = User.find_for_open_id(env['omniauth.auth'], current_user)
     if @user.persisted?
-      #flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Google"
       if @user.approved?
         sign_in_and_redirect( @user, :event => :authentication )
       else
-        flash[:notice] = "Your invite request is currently pending."
+        flash[:notice] = 'Your invite request is currently pending.'
         redirect_to( root_path )
       end
     else
-      session["devise.open:id_data"] = env["openid.ext1"]
+      session['devise.open:id_data'] = env['openid.ext1']
       redirect_to new_user_registration_url
     end
   end
